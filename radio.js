de6dabae5d5fcaa8e6d917ff0872dda1fee55b78
RittiellyMangarotti <141073496+RittiellyMangarotti@users.noreply.github.com>
2023-09-25 14:04:22 -0400

    Update radio.js

--- radio.js
+++ radio.js
@@ -4,6 +4,17 @@ const prevButton = document.getElementById('prev-button');
 const nextButton = document.getElementById('next-button');
 
 const songs = [
+    'musicas/musica 1.mp3',
+    'musicas/musica 2.mp3',
+    'musicas/musica 3.mp3',
+    'musicas/musica 4.mp3',
+    'musicas/musica 5.mp3',
+    'musicas/musica 6.mp3',
+    'musicas/musica 7.mp3',
+    'musicas/musica 8.mp3',
+    'musicas/musica 9.mp3',
+    'musicas/musica 10.mp3',
+    
     
 ];
 
