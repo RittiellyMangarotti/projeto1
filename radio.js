fb5b83bc5b7ac518c704fcca22cd309499a38bbf
RittiellyMangarotti <141073496+RittiellyMangarotti@users.noreply.github.com>
2023-09-25 13:52:30 -0400

    Update radio.js

--- radio.js
+++ radio.js
@@ -6,16 +6,6 @@ const nextButton = document.getElementById('next-button');
 const songs = [
     'radiomp3/musica 1.mp3',
     
-    'radiomp3/Como Nossos Pais(MP3_160K).mp3',
-    'radiomp3/EVIL(MP3_160K).mp3',
-    'radiomp3/Foi Mal(MP3_160K).mp3',
-    'radiomp3/Giant Woman (feat. Zach Callison)(MP3_160K).mp3',
-    'radiomp3/Greta Van Fleet - Light My Love (Audio)(MP3_160K).mp3',
-    'radiomp3/Infinity Pools(MP3_160K).mp3',
-    'radiomp3/The Principal(MP3_160K).mp3',
-    'radiomp3/The Technicolors - Howl (Official Music Video)(MP3_160K).mp3',
-    'radiomp3/URIAS - DANGER ( OFFICIAL VISUALIZER)(MP3_160K).mp3',
-    'radiomp3/Wait a Minute_(MP3_160K).mp3',
 ];
 
 let currentSongIndex = 0;
